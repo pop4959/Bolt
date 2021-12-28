1dce9f0b74bddbb049cf8c95b874d144fcfdd116
pop4959 <pop4959@gmail.com>
2021-12-28 14:41:51 -0800

    Update gradle and java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ subprojects {
 
     java {
         toolchain {
-            languageVersion.set(JavaLanguageVersion.of(16))
+            languageVersion.set(JavaLanguageVersion.of(17))
         }
         withSourcesJar()
     }
