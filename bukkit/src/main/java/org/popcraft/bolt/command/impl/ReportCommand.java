df7c35beabc6438cdfe0a701dbcda0756061cc65
pop4959 <pop4959@gmail.com>
2022-01-27 12:59:56 -0800

    More advanced reports

--- bukkit/src/main/java/org/popcraft/bolt/command/impl/ReportCommand.java
+++ bukkit/src/main/java/org/popcraft/bolt/command/impl/ReportCommand.java
@@ -1,5 +1,8 @@
 package org.popcraft.bolt.command.impl;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.minimessage.Template;
 import org.bukkit.command.CommandSender;
 import org.popcraft.bolt.BoltPlugin;
 import org.popcraft.bolt.command.Arguments;
@@ -8,18 +11,40 @@ import org.popcraft.bolt.util.BoltComponents;
 import org.popcraft.bolt.util.Metrics;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ReportCommand extends BoltCommand {
+    private long previousHits, previousMisses;
+    private Map<Metrics.ProtectionAccess, Long> previousExecutionCounts;
+
     public ReportCommand(BoltPlugin plugin) {
         super(plugin);
     }
 
     @Override
     public void execute(CommandSender sender, Arguments arguments) {
-        BoltComponents.sendMessage(sender, "Hits: %d".formatted(Metrics.getProtectionHits()));
-        BoltComponents.sendMessage(sender, "Misses: %d".formatted(Metrics.getProtectionHits()));
-        Metrics.getProtectionAccesses().forEach((name, count) -> BoltComponents.sendMessage(sender, "%s: %d".formatted(name, count)));
+        final long hits = Metrics.getProtectionHits();
+        final long misses = Metrics.getProtectionMisses();
+        final boolean hitsChanged = hits != previousHits;
+        final boolean missesChanged = misses != previousMisses;
+        BoltComponents.sendMessage(sender, "Hits: <hits>", Template.of("hits", Component.text(hits).color(hitsChanged ? NamedTextColor.RED : NamedTextColor.WHITE)));
+        BoltComponents.sendMessage(sender, "Misses: <misses>", Template.of("misses", Component.text(misses).color(missesChanged ? NamedTextColor.RED : NamedTextColor.WHITE)));
+        final Map<Metrics.ProtectionAccess, Long> protectionAccessCounts = Metrics.getProtectionAccessCounts();
+        final Map<String, List<Metrics.ProtectionAccess>> protectionAccessListByType = protectionAccessCounts.keySet().stream().collect(Collectors.groupingBy(Metrics.ProtectionAccess::type, Collectors.toList()));
+        protectionAccessListByType.forEach((type, list) -> {
+            BoltComponents.sendMessage(sender, "<type>", Template.of("type", type));
+            list.forEach(protectionAccess -> {
+                final long count = protectionAccessCounts.get(protectionAccess);
+                final boolean countChanged = previousExecutionCounts != null && count != previousExecutionCounts.getOrDefault(protectionAccess, 0L);
+                BoltComponents.sendMessage(sender, "<consumer>: <count>", Template.of("consumer", protectionAccess.consumer()), Template.of("count", Component.text(count).color(countChanged ? NamedTextColor.RED : NamedTextColor.WHITE)));
+            });
+        });
+        this.previousHits = hits;
+        this.previousMisses = misses;
+        this.previousExecutionCounts = new HashMap<>(protectionAccessCounts);
     }
 
     @Override
--- bukkit/src/main/java/org/popcraft/bolt/util/Metrics.java
+++ bukkit/src/main/java/org/popcraft/bolt/util/Metrics.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class Metrics {
-    private static final Map<String, Long> protectionAccesses = new HashMap<>();
+    private static final String REVERSE_DOMAIN = "org.popcraft";
+    private static final String STORE_PACKAGE = REVERSE_DOMAIN + ".bolt.store";
+    private static final Map<ProtectionAccess, Long> protectionAccessCounts = new HashMap<>();
     private static long protectionHits;
     private static long protectionMisses;
 
@@ -18,14 +20,26 @@ public final class Metrics {
             ++protectionMisses;
         }
         final StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        if (stackTrace.length > 3) {
-            final String method = stackTrace[3].getMethodName();
-            protectionAccesses.put(method, protectionAccesses.getOrDefault(method, 0L) + 1);
+        for (int i = 0; i < stackTrace.length; ++i) {
+            final StackTraceElement stackTraceElement = stackTrace[i];
+            if (stackTraceElement.getClassName().startsWith(STORE_PACKAGE)) {
+                final String type = stackTraceElement.getMethodName();
+                for (int j = i + 1; j < stackTrace.length - 1; ++j) {
+                    final StackTraceElement nextStackTraceElement = stackTrace[j + 1];
+                    if (!nextStackTraceElement.getClassName().startsWith(REVERSE_DOMAIN)) {
+                        final String consumer = stackTrace[j].getMethodName();
+                        final ProtectionAccess protectionAccess = new ProtectionAccess(type, consumer);
+                        protectionAccessCounts.put(protectionAccess, protectionAccessCounts.getOrDefault(protectionAccess, 0L) + 1);
+                        return;
+                    }
+                }
+                break;
+            }
         }
     }
 
-    public static Map<String, Long> getProtectionAccesses() {
-        return protectionAccesses;
+    public static Map<ProtectionAccess, Long> getProtectionAccessCounts() {
+        return protectionAccessCounts;
     }
 
     public static long getProtectionHits() {
@@ -34,5 +48,8 @@ public final class Metrics {
 
     public static long getProtectionMisses() {
         return protectionMisses;
+    }
+
+    public record ProtectionAccess(String type, String consumer) {
     }
 }
