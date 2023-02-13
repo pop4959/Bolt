e1ac1bf801f1f9e764d9e73e709536e400a716c4
Christopher Bohn <pop4959@gmail.com>
2023-02-12 20:52:50 -0800

    remove store timer

--- bukkit/src/main/java/org/popcraft/bolt/data/SimpleProtectionCache.java
+++ bukkit/src/main/java/org/popcraft/bolt/data/SimpleProtectionCache.java
@@ -31,18 +31,8 @@ public class SimpleProtectionCache implements Store {
 
     @Override
     public BlockProtection loadBlockProtection(BlockLocation location) {
-        final long startTimeNanos = System.nanoTime();
-
         final BlockProtection blockProtection = cachedBlocks.get(location);
-
-        final boolean hit = blockProtection != null;
-        if (hit) {
-            final long timeNanos = System.nanoTime() - startTimeNanos;
-            final double timeMillis = timeNanos / 1e6d;
-            LogManager.getLogManager().getLogger("").info(() -> "Loading block protection took %.3f ms".formatted(timeMillis));
-        }
-        Metrics.recordProtectionAccess(hit);
-
+        Metrics.recordProtectionAccess(blockProtection != null);
         return blockProtection;
     }
 
@@ -53,42 +43,20 @@ public class SimpleProtectionCache implements Store {
 
     @Override
     public void saveBlockProtection(BlockProtection protection) {
-        final long startTimeNanos = System.nanoTime();
-
         cachedBlocks.put(BukkitAdapter.blockLocation(protection), protection);
         CompletableFuture.runAsync(() -> backingStore.saveBlockProtection(protection), executorThread);
-
-        final long timeNanos = System.nanoTime() - startTimeNanos;
-        final double timeMillis = timeNanos / 1e6d;
-        LogManager.getLogManager().getLogger("").info(() -> "Saving block protection took %.3f ms".formatted(timeMillis));
     }
 
     @Override
     public void removeBlockProtection(BlockProtection protection) {
-        final long startTimeNanos = System.nanoTime();
-
         cachedBlocks.remove(BukkitAdapter.blockLocation(protection));
         CompletableFuture.runAsync(() -> backingStore.removeBlockProtection(protection), executorThread);
-
-        final long timeNanos = System.nanoTime() - startTimeNanos;
-        final double timeMillis = timeNanos / 1e6d;
-        LogManager.getLogManager().getLogger("").info(() -> "Removing block protection took %.3f ms".formatted(timeMillis));
     }
 
     @Override
     public EntityProtection loadEntityProtection(UUID id) {
-        final long startTimeNanos = System.nanoTime();
-
         final EntityProtection entityProtection = cachedEntities.get(id);
-
-        final boolean hit = entityProtection != null;
-        if (hit) {
-            final long timeNanos = System.nanoTime() - startTimeNanos;
-            final double timeMillis = timeNanos / 1e6d;
-            LogManager.getLogManager().getLogger("").info(() -> "Loading entity protection took %.3f ms".formatted(timeMillis));
-        }
-        Metrics.recordProtectionAccess(hit);
-
+        Metrics.recordProtectionAccess(entityProtection != null);
         return entityProtection;
     }
 
@@ -99,25 +67,13 @@ public class SimpleProtectionCache implements Store {
 
     @Override
     public void saveEntityProtection(EntityProtection protection) {
-        final long startTimeNanos = System.nanoTime();
-
         cachedEntities.put(protection.getId(), protection);
         CompletableFuture.runAsync(() -> backingStore.saveEntityProtection(protection), executorThread);
-
-        final long timeNanos = System.nanoTime() - startTimeNanos;
-        final double timeMillis = timeNanos / 1e6d;
-        LogManager.getLogManager().getLogger("").info(() -> "Saving entity protection took %.3f ms".formatted(timeMillis));
     }
 
     @Override
     public void removeEntityProtection(EntityProtection protection) {
-        final long startTimeNanos = System.nanoTime();
-
         cachedEntities.remove(protection.getId());
         CompletableFuture.runAsync(() -> backingStore.removeEntityProtection(protection), executorThread);
-
-        final long timeNanos = System.nanoTime() - startTimeNanos;
-        final double timeMillis = timeNanos / 1e6d;
-        LogManager.getLogManager().getLogger("").info(() -> "Removing entity protection took %.3f ms".formatted(timeMillis));
     }
 }
