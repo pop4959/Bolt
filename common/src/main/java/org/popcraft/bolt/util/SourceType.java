fdf7383535f202f1bc110845bafe26b80875e4d2
Christopher Bohn <pop4959@gmail.com>
2023-02-28 01:37:00 -0800

    rearrange default source types

--- common/src/main/java/org/popcraft/bolt/util/SourceType.java
+++ common/src/main/java/org/popcraft/bolt/util/SourceType.java
@@ -5,11 +5,12 @@ public final class SourceType {
     }
 
     public static final String PLAYER = "player";
-    public static final String GROUP = "group";
-    public static final String PERMISSION = "permission";
     public static final String PASSWORD = "password";
+    public static final String PERMISSION = "permission";
+    public static final String GROUP = "group";
     public static final String REDSTONE = "redstone";
     public static final String BLOCK = "block";
-    public static final String TOWN = "town";
     public static final String REGION = "region";
+    public static final String TOWN = "town";
+    public static final String FACTION = "faction";
 }
