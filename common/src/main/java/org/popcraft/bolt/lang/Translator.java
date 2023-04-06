89cff3e15e786212af67d662ce193348dfc7f8e7
Christopher Bohn <pop4959@gmail.com>
2023-04-05 22:25:58 -0700

    Improve translator

--- common/src/main/java/org/popcraft/bolt/lang/Translator.java
+++ common/src/main/java/org/popcraft/bolt/lang/Translator.java
@@ -12,30 +12,29 @@ import java.util.Properties;
 
 public final class Translator {
     private static final String TRANSLATION_FILE_FORMAT = "lang/%s.properties";
-    private static final String DEFAULT_LOCALE_FORMAT = "%s_%s";
     private static final Properties fallback = loadTranslation("en");
-    private static Properties translations = loadTranslation("en");
+    private static Properties translation = loadTranslation("en");
+    private static Properties customTranslation = loadTranslation("en");
     private static String selected = "en";
 
     private Translator() {
     }
 
     public static String translate(final String key) {
-        return Objects.requireNonNullElse(translations.getProperty(key), Objects.requireNonNullElse(fallback.getProperty(key), key));
+        return Objects.requireNonNullElseGet(customTranslation.getProperty(key), () -> Objects.requireNonNullElseGet(translation.getProperty(key), () -> Objects.requireNonNullElse(fallback.getProperty(key), key)));
     }
 
     public static void load(final Path directory, final String language) {
-        if (language == null) {
-            return;
+        translation = loadTranslation(language);
+        final Path filePath = directory.resolve(language + ".properties");
+        customTranslation = loadTranslationFromFile(filePath);
+        if (!customTranslation.isEmpty()) {
+            selected = "custom";
+        } else if (!translation.isEmpty()) {
+            selected = language;
+        } else {
+            selected = "en";
         }
-        if (directory != null) {
-            final Path filePath = directory.resolve(language + ".properties");
-            if (Files.exists(filePath)) {
-                translations = loadTranslationFromFile(filePath);
-                return;
-            }
-        }
-        translations = loadTranslation(language);
     }
 
     public static String selected() {
@@ -43,30 +42,25 @@ public final class Translator {
     }
 
     private static Properties loadTranslation(final String language) {
-        final Properties properties = new Properties();
         final ClassLoader classLoader = Translator.class.getClassLoader();
-        String newSelected = language;
-        InputStream input = classLoader.getResourceAsStream(TRANSLATION_FILE_FORMAT.formatted(language));
-        if (input == null) {
-            newSelected = DEFAULT_LOCALE_FORMAT.formatted(language.toLowerCase(), language.toUpperCase());
-            input = classLoader.getResourceAsStream(TRANSLATION_FILE_FORMAT.formatted(newSelected));
-        }
-        if (input != null) {
-            try (final BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))) {
-                properties.load(reader);
-                selected = newSelected;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        final Properties properties = new Properties();
+        final String translationFile = TRANSLATION_FILE_FORMAT.formatted(language);
+        try (final InputStream input = Objects.requireNonNullElseGet(classLoader.getResourceAsStream(translationFile), InputStream::nullInputStream);
+             final BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))) {
+            properties.load(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return properties;
     }
 
     private static Properties loadTranslationFromFile(final Path path) {
         final Properties properties = new Properties();
+        if (!Files.exists(path)) {
+            return properties;
+        }
         try (final BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
             properties.load(reader);
-            selected = "custom";
         } catch (IOException e) {
             e.printStackTrace();
         }
