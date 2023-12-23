bce1fd190409a68e05f5747dbecc9545cd054a3b
Christopher Bohn <pop4959@gmail.com>
2023-12-23 03:09:47 -0800

    Update to 1.20.4

--- bukkit/src/main/java/org/popcraft/bolt/matcher/block/GrassMatcher.java
+++ bukkit/src/main/java/org/popcraft/bolt/matcher/block/GrassMatcher.java
@@ -5,14 +5,27 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.popcraft.bolt.matcher.Match;
+import org.popcraft.bolt.util.EnumUtil;
 
 import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Set;
 
 public class GrassMatcher implements BlockMatcher {
-    private static final EnumSet<Material> GRASS = EnumSet.of(Material.GRASS, Material.FERN, Material.SEAGRASS, Material.NETHER_SPROUTS, Material.WARPED_ROOTS, Material.CRIMSON_ROOTS);
+    private static final Material SHORT_GRASS_OLD = EnumUtil.valueOf(Material.class, "GRASS").orElse(null);
+    private static final Material SHORT_GRASS = EnumUtil.valueOf(Material.class, "SHORT_GRASS").orElse(null);
+    private static final EnumSet<Material> GRASS = EnumSet.of(Material.FERN, Material.SEAGRASS, Material.NETHER_SPROUTS, Material.WARPED_ROOTS, Material.CRIMSON_ROOTS);
     private boolean enabled;
+
+    static {
+        // Future: Replace with Material.SHORT_GRASS
+        if (SHORT_GRASS_OLD != null) {
+            GRASS.add(SHORT_GRASS_OLD);
+        }
+        if (SHORT_GRASS != null) {
+            GRASS.add(SHORT_GRASS);
+        }
+    }
 
     @Override
     public void initialize(Set<Material> protectableBlocks, Set<EntityType> protectableEntities) {
