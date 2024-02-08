88d31a732368289daf73add9f94f965b0727561b
Emilia Kond <emilia@rymiel.space>
2024-02-08 06:06:30 +0200

    rename

--- bukkit/src/main/java/org/popcraft/bolt/BoltPlugin.java
+++ bukkit/src/main/java/org/popcraft/bolt/BoltPlugin.java
@@ -758,7 +758,7 @@ public class BoltPlugin extends JavaPlugin implements BoltAPI {
                 }
             }
         }
-        unresolved.removeIf(permission -> sourceResolver.resolve(Source.of(SourceTypes.PERMISSION, "bolt.bypass." + permission)));
+        unresolved.removeIf(permission -> sourceResolver.resolve(Source.of(SourceTypes.PERMISSION, "bolt.permission." + permission)));
         return unresolved.isEmpty();
     }
 
