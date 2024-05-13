f01ec7996e5cbd78c571e95248f3e40f8a96ac53
Christopher Bohn <pop4959@gmail.com>
2024-05-12 22:00:57 -0700

    use wool carpet tag

--- bukkit/src/main/java/org/popcraft/bolt/matcher/block/CarpetMatcher.java
+++ bukkit/src/main/java/org/popcraft/bolt/matcher/block/CarpetMatcher.java
@@ -1,6 +1,7 @@
 package org.popcraft.bolt.matcher.block;
 
 import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
@@ -10,17 +11,11 @@ import java.util.Collections;
 import java.util.Set;
 
 public class CarpetMatcher implements BlockMatcher {
-    // Future: Replace with Tag.WOOL_CARPETS
-    private static final Set<Material> WOOL_CARPETS = Set.of(Material.WHITE_CARPET, Material.ORANGE_CARPET,
-            Material.MAGENTA_CARPET, Material.LIGHT_BLUE_CARPET, Material.YELLOW_CARPET, Material.LIME_CARPET,
-            Material.PINK_CARPET, Material.GRAY_CARPET, Material.LIGHT_GRAY_CARPET, Material.CYAN_CARPET,
-            Material.PURPLE_CARPET, Material.BLUE_CARPET, Material.BROWN_CARPET, Material.GREEN_CARPET,
-            Material.RED_CARPET, Material.BLACK_CARPET);
     private boolean enabled;
 
     @Override
     public void initialize(Set<Material> protectableBlocks, Set<EntityType> protectableEntities) {
-        enabled = protectableBlocks.stream().anyMatch(WOOL_CARPETS::contains);
+        enabled = protectableBlocks.stream().anyMatch(Tag.WOOL_CARPETS::isTagged);
     }
 
     @Override
@@ -36,7 +31,7 @@ public class CarpetMatcher implements BlockMatcher {
     @Override
     public Match findMatch(Block block) {
         final Block above = block.getRelative(BlockFace.UP);
-        if (WOOL_CARPETS.contains(above.getType())) {
+        if (Tag.WOOL_CARPETS.isTagged(above.getType())) {
             return Match.ofBlocks(Collections.singleton(above));
         }
         return null;
