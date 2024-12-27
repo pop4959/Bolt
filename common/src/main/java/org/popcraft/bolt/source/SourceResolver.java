4a102a1548e6528c61137a87b2b9c1bc8d8874e5
Emilia Kond <emilia@rymiel.space>
2024-12-27 20:23:52 +0200

    Try to clarify things around sources

--- common/src/main/java/org/popcraft/bolt/source/SourceResolver.java
+++ common/src/main/java/org/popcraft/bolt/source/SourceResolver.java
@@ -1,8 +1,8 @@
 package org.popcraft.bolt.source;
 
 /**
- * A source resolver is something that can try to access a protection. It is passed sources, and returns whether the given
- * source is applicable to this source resolver.
+ * A source resolver is something that can try to access a protection. It is passed a {@link Source source}, and returns
+ * whether that source is applicable to this source resolver.
  * <p>
  * For example, a source resolver representing a player would return {@code true} when given a player source with the
  * player's UUID. Or a group source with a group that the player is part of.
