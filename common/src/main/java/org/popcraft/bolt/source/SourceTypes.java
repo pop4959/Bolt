624ad9e2b625da441f3312c0a1e7bdc4c07b9366
Emilia Kond <emilia@rymiel.space>
2024-12-27 16:53:18 +0200

    Document the most common source types

--- common/src/main/java/org/popcraft/bolt/source/SourceTypes.java
+++ common/src/main/java/org/popcraft/bolt/source/SourceTypes.java
@@ -4,12 +4,33 @@ public final class SourceTypes {
     private SourceTypes() {
     }
 
+    /**
+     * Represents a player. The identifier is the UUID of the player
+     */
     public static final String PLAYER = "player";
+    /**
+     * Represents anyone who knows a password. The identifier is the SHA-1 hash of the actual password.
+     */
     public static final String PASSWORD = "password";
+    /**
+     * Represents anyone with the permission node in the identifier.
+     */
     public static final String PERMISSION = "permission";
+    /**
+     * Represents anyone part of a Bolt group ({@code /bolt group}).
+     */
     public static final String GROUP = "group";
+    /**
+     * Represents a redstone signal. Identifier does not matter.
+     */
     public static final String REDSTONE = "redstone";
+    /**
+     * Represents a block. Identifier does not matter.
+     */
     public static final String BLOCK = "block";
+    /**
+     * Represents a door. Used for autoclose. Identifier does not matter.
+     */
     public static final String DOOR = "door";
     public static final String REGION = "region";
     public static final String TOWN = "town";
