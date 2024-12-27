50aa34ad60ff0f4bc0545426e47b56e7dc8549dd
Emilia Kond <emilia@rymiel.space>
2024-12-27 20:15:30 +0200

    typo

--- common/src/main/java/org/popcraft/bolt/source/SourceResolver.java
+++ common/src/main/java/org/popcraft/bolt/source/SourceResolver.java
@@ -1,7 +1,7 @@
 package org.popcraft.bolt.source;
 
 /**
- * A source resolver is something that can try to access a location. It is passed sources, and returns whether the given
+ * A source resolver is something that can try to access a protection. It is passed sources, and returns whether the given
  * source is applicable to this source resolver.
  * <p>
  * For example, a source resolver representing a player would return {@code true} when given a player source with the
