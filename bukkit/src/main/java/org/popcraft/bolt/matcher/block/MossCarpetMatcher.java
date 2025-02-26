ed99955a36fc673db820af326a17ac9de15eb7c4
Emilia Kond <emilia@rymiel.space>
2025-02-26 08:06:23 +0200

    Pale moss carpet

--- bukkit/src/main/java/org/popcraft/bolt/matcher/block/MossCarpetMatcher.java
+++ bukkit/src/main/java/org/popcraft/bolt/matcher/block/MossCarpetMatcher.java
@@ -1,20 +1,32 @@
 package org.popcraft.bolt.matcher.block;
 
+import com.google.common.collect.Sets;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.popcraft.bolt.matcher.Match;
+import org.popcraft.bolt.util.EnumUtil;
 
 import java.util.Collections;
 import java.util.Set;
 
 public class MossCarpetMatcher implements BlockMatcher {
+    // Future: make immutable (Set.of)
+    private static final Set<Material> MOSS_CARPETS = Sets.newHashSet(Material.MOSS_CARPET);
+    private static final Material PALE_MOSS_CARPET = EnumUtil.valueOf(Material.class, "PALE_MOSS_CARPET").orElse(null);
     private boolean enabled;
+
+    static {
+        // Future: Replace with Material.PALE_MOSS_CARPET
+        if (PALE_MOSS_CARPET != null) {
+            MOSS_CARPETS.add(PALE_MOSS_CARPET);
+        }
+    }
 
     @Override
     public void initialize(Set<Material> protectableBlocks, Set<EntityType> protectableEntities) {
-        enabled = protectableBlocks.contains(Material.MOSS_CARPET);
+        enabled = protectableBlocks.stream().anyMatch(MOSS_CARPETS::contains);
     }
 
     @Override
@@ -30,7 +42,7 @@ public class MossCarpetMatcher implements BlockMatcher {
     @Override
     public Match findMatch(Block block) {
         final Block above = block.getRelative(BlockFace.UP);
-        if (Material.MOSS_CARPET.equals(above.getType())) {
+        if (MOSS_CARPETS.contains(above.getType())) {
             return Match.ofBlocks(Collections.singleton(above));
         }
         return null;
