c3baccfc81537ae32f8e64f5e468bcfd81f8d728
Emilia Kond <emilia@rymiel.space>
2025-02-26 07:58:21 +0200

    Pale hanging moss

--- bukkit/src/main/java/org/popcraft/bolt/matcher/block/HangingVineMatcher.java
+++ bukkit/src/main/java/org/popcraft/bolt/matcher/block/HangingVineMatcher.java
@@ -6,17 +6,31 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.EntityType;
 import org.popcraft.bolt.matcher.Match;
+import org.popcraft.bolt.util.EnumUtil;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class HangingVineMatcher implements BlockMatcher {
     private static final Set<Material> WEEPING_VINES = Set.of(Material.WEEPING_VINES, Material.WEEPING_VINES_PLANT);
+    private static final Material PALE_HANGING_MOSS = EnumUtil.valueOf(Material.class, "PALE_HANGING_MOSS").orElse(null);
     private boolean enabled;
+
+    private boolean isHangingVine(Material material) {
+        if (Tag.CAVE_VINES.isTagged(material) || WEEPING_VINES.contains(material)) {
+            return true;
+        }
+        // Future: Replace with Material.PALE_HANGING_MOSS and put into the set with weeping vines
+        if (PALE_HANGING_MOSS == null) {
+            return false;
+        } else {
+            return PALE_HANGING_MOSS.equals(material);
+        }
+    }
 
     @Override
     public void initialize(Set<Material> protectableBlocks, Set<EntityType> protectableEntities) {
-        enabled = protectableBlocks.stream().anyMatch(material -> Tag.CAVE_VINES.isTagged(material) || WEEPING_VINES.contains(material));
+        enabled = protectableBlocks.stream().anyMatch(this::isHangingVine);
     }
 
     @Override
@@ -32,9 +46,9 @@ public class HangingVineMatcher implements BlockMatcher {
     @Override
     public Match findMatch(Block block) {
         final Block below = block.getRelative(BlockFace.DOWN);
-        if (Tag.CAVE_VINES.isTagged(block.getType()) || Tag.CAVE_VINES.isTagged(below.getType()) || WEEPING_VINES.contains(block.getType()) || WEEPING_VINES.contains(below.getType())) {
+        if (isHangingVine(block.getType()) || isHangingVine(below.getType())) {
             final Set<Block> blocks = new HashSet<>();
-            for (Block next = block.getRelative(BlockFace.DOWN); Tag.CAVE_VINES.isTagged(next.getType()) || WEEPING_VINES.contains(next.getType()); next = next.getRelative(BlockFace.DOWN)) {
+            for (Block next = block.getRelative(BlockFace.DOWN); isHangingVine(next.getType()); next = next.getRelative(BlockFace.DOWN)) {
                 blocks.add(next);
             }
             return Match.ofBlocks(blocks);
